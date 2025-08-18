62a97a724f5e19442fb9b4a4979869b4405430e5
ilyas lokhand <ilokhandwala786@gmail.com>
2025-08-18 12:32:11 +0530

    feat: update package configurations and Axios base URL for environment handling

--- Frontend/src/Lib/Axios.js
+++ Frontend/src/Lib/Axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api";
+
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: BASE_URL,
   withCredentials:true,
 })
 
